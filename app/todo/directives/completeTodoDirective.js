2b761854f87fa6e2fe280f98b4ed093abedbc4ac
freemany <freeman_yam@hotmail.com>
2020-05-19 17:08:04 +1000

    complete-todo tag directives

--- app/todo/directives/completeTodoDirective.js
+++ app/todo/directives/completeTodoDirective.js
@@ -1,16 +1,10 @@
 const CompleteTodoDirective = () => {
     return {
-        restrict: 'A',
-        bindToController: true,
+        restrict: 'E',
         templateUrl: './app/todo/directives/completeTodoDirective.html',
-        // scope: {
-        //     todos: '=todos',
-        // },
-        // link(scope) {
-        //     console.log(scope.todos);
-        //     scope.completedTodos = scope.todos;
-        //     // scope.todos = scope.todos.filter(t => true === t.done);
-        // },
+        scope: {
+            todos: '=',
+        },
         controller: class CompleteTodoDirectiveCtrl {
             constructor($parse, $scope, $element, $attrs) {
                 'ngInject';
@@ -19,8 +13,7 @@ const CompleteTodoDirective = () => {
                 this.$element = $element;
                 this.$attrs = $attrs;
                 this.$scope.label = 'Completed:';
-                console.log(this.$scope.ctrl, this.$attrs.completeTodo);
-                this.$scope.$watch(this.$attrs.completeTodo, (n, o) => { console.log(n)
+                this.$scope.$watch('todos', (n, o) => { 
                     if (n) {
                         this.$scope.completedTodos = n.filter(t => t.done);
                     }
